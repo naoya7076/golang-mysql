40af45c5c5abc4ef08fff66b73390cd23f3b12e8
naoya7076 <naoya7076@gmail.com>
2023-01-08 18:26:22 +0900

    add createTable.sql

---
+++ createTable.sql
@@ -0,0 +1,19 @@
+create table
+IF not EXISTS articles
+(
+  article_id integer unsigned auto_increment primary key,
+  title VARCHAR (100) NOT NULL,
+  contents text not NULL,
+  username VARCHAR (100) not NULL,
+  nice INTEGER NOT NULL,
+  created_at DATETIME
+);
+
+create table if not EXISTS comments(
+  comment_id integer unsigned auto_increment primary key,
+  article_id integer unsigned not NULL,
+  message text not NULL,
+  created_at DATETIME,
+  foreign key (article_id) refernce articles(article_id)  
+)
+
